343480a663a600082152c40dac4f5cfe70eae711
morteza mirshekar <asreweb81@gmail.com>
2023-04-07 07:31:05 +0430

    [BUGFIX] Make some changes!

--- Softmax Libraries/Softmax.Result/Pagination.cs
+++ Softmax Libraries/Softmax.Result/Pagination.cs
@@ -2,10 +2,10 @@
 {
 	public class Pagination
 	{
-		public int CurrentPage { get; internal set; }
-		public int TotalPages { get; internal set; }
-		public int PageSize { get; internal set; }
-		public int TotalCount { get; internal set; }
+		public int CurrentPage { get; set; }
+		public int TotalPages { get; set; }
+		public int PageSize { get; set; }
+		public int TotalCount { get; set; }
 
 		public bool HasPrevious => CurrentPage > 1;
 		public bool HasNext => CurrentPage < TotalPages;
