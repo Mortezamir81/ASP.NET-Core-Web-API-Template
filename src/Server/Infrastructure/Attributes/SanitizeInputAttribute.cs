cf9ef6cf99c08f175a3c56684d76f385d054a8fd
mirshekarm <asreweb81@gmail.com>
2023-08-13 01:58:04 +0330

    [FEATURE] Add SanitizeInputAttribute!

---
+++ src/Server/Infrastructure/Attributes/SanitizeInputAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Ganss.Xss;
+
+namespace Infrastructure.Attributes;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+public class SanitizeInputAttribute : ActionFilterAttribute
+{
+	public override void OnActionExecuting(ActionExecutingContext filterContext)
+	{
+		if (filterContext is null)
+		{
+			return;
+		}
+
+		var sanitizer =
+			filterContext.HttpContext.RequestServices.GetRequiredService<IHtmlSanitizer>();
+
+		filterContext.CleanupActionStringValues(data => sanitizer.Sanitize(data));
+	}
+}
